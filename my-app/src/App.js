1fac12f43966a3e60112b5d24281fd7ba0d42711
Neil <z5202dbc@gmail.com>
2020-01-07 10:18:14 -0800

    Try to create new index.js and unuse App.js for now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
@@ -22,5 +23,21 @@ function App() {
     </div>
   );
 }
+
+// // 更新 UI
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('child'));
+// }
+
+// // 每秒更新一次
+// setInterval(tick, 1000);
+
+// export default tick;
 
 export default App;
